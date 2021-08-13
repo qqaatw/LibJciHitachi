c28c23a588e47ba1aebd7584e605dbd18a248c05
qqaatw <qqaatw@gmail.com>
2021-08-13 14:20:15 +0800

    Add source code button.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-import JciHitachi
 print(sys.path)
 
 # -- Project information -----------------------------------------------------
@@ -32,7 +31,7 @@ release = 'v0.0.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
