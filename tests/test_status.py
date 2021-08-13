5be6795fc292c83aa25e21d4f3769b7f2dfe1cbb
qqaatw <qqaatw@gmail.com>
2021-08-13 20:29:07 +0800

    Add AC status correctness test.

--- tests/test_status.py
+++ tests/test_status.py
@@ -25,6 +25,31 @@ class TestACStatus:
         ac_status = JciHitachiAC(dev_status).status
         for each_stat in ac_status.values():
             assert each_stat == "unsupported" or each_stat == -1
+    
+    def test_correctness(self):
+        dev_status = JciHitachiStatusInterpreter(MOCK_CODE).decode_status()
+        ac_status = JciHitachiAC(dev_status).status
+
+        mock_status = {
+            'power': 'off',
+            'mode': 'cool',
+            'air_speed': 'silent',
+            'target_temp': 26,
+            'indoor_temp': 26,
+            'sleep_timer': 0,
+            'vertical_wind_swingable': 'unsupported',
+            'vertical_wind_direction': -1,
+            'horizontal_wind_direction': -1,
+            'mold_prev': 'disabled',
+            'fast_op': 'disabled',
+            'energy_save': 'disabled',
+            'sound_prompt': 'enabled',
+            'outdoor_temp': 24
+        }
+
+        for key, value in ac_status.items():
+            assert key in mock_status
+            assert mock_status[key] == value
 
     def test_ac_online(self):
         api = JciHitachiAPI(
