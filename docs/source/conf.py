abe2c441e36ceb0dd092a7d464d322245683f63b
qqaatw <qqaatw@gmail.com>
2022-06-12 15:27:51 +0800

    Bump version to 0.5.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Allan Lin'
 author = 'Allan Lin'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.5.1'
+release = 'v0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
